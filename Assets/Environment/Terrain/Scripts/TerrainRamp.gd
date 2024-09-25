e50f5069f12725c347f462bc7e09c9eb39e0d747
TBoneHolmes <142822551+TBoneHolmes@users.noreply.github.com>
2024-09-25 12:57:24 -0700

    Completed ramp terrain prototype

--- Assets/Environment/Terrain/Scripts/TerrainRamp.gd
+++ Assets/Environment/Terrain/Scripts/TerrainRamp.gd
@@ -17,26 +17,152 @@ func _ready():
 	_generate_collision()
 
 
-
 func _process(delta):
 	pass
-
 
 
 func _generate_collision():
 	var vertices : PackedVector3Array
 	var shape : ConcavePolygonShape3D = ConcavePolygonShape3D.new()
+	var t_size : Vector2 = Vector2(size.x, size.z) / 2
 	
-	####### TOP
-	### X+, Z+
+	
+	######### TOP
+	#### X+, Z+
 	## Tri+
-	vertices.push_back(Vector3(0, 5, 4))
-	vertices.push_back(Vector3(4, 5, 0))
-	vertices.push_back(Vector3(4, 5, 4))
+	vertices.push_back(Vector3(0, rampHeightPoints[1], t_size.y))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[3], 0))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[0], t_size.y))
 	## Tri-
-	vertices.push_back(Vector3(0, 5, 0))
-	vertices.push_back(Vector3(4, 5, 0))
-	vertices.push_back(Vector3(0, 5, 4))
+	vertices.push_back(Vector3(0, rampHeightPoints[4], 0))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[3], 0))
+	vertices.push_back(Vector3(0, rampHeightPoints[1], t_size.y))
+	### X-, Z+
+	## Tri+
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[2], t_size.y))
+	vertices.push_back(Vector3(0, rampHeightPoints[4], 0))
+	vertices.push_back(Vector3(0, rampHeightPoints[1], t_size.y))
+	## Tri-
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[5], 0))
+	vertices.push_back(Vector3(0, rampHeightPoints[4], 0))
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[2], t_size.y))
+	#### X+, Z-
+	## Tri+
+	vertices.push_back(Vector3(0, rampHeightPoints[4], 0))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[6], -t_size.y))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[3], 0))
+	## Tri-
+	vertices.push_back(Vector3(0, rampHeightPoints[7], -t_size.y))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[6], -t_size.y))
+	vertices.push_back(Vector3(0, rampHeightPoints[4], 0))
+	### X-, Z-
+	## Tri+
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[5], 0))
+	vertices.push_back(Vector3(0, rampHeightPoints[7], -t_size.y))
+	vertices.push_back(Vector3(0, rampHeightPoints[4], 0))
+	## Tri-
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[8], -t_size.y))
+	vertices.push_back(Vector3(0, rampHeightPoints[7], -t_size.y))
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[5], 0))
+	
+	######### FRONT
+	#### X+
+	## X+
+	vertices.push_back(Vector3(0, rampHeightPoints[7], -t_size.y))
+	vertices.push_back(Vector3(t_size.x, 0, -t_size.y))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[6], -t_size.y))
+	## X-
+	vertices.push_back(Vector3(0, 0, -t_size.y))
+	vertices.push_back(Vector3(t_size.x, 0, -t_size.y))
+	vertices.push_back(Vector3(0, rampHeightPoints[7], -t_size.y))
+	#### X-
+	## X+
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[8], -t_size.y))
+	vertices.push_back(Vector3(0, 0, -t_size.y))
+	vertices.push_back(Vector3(0, rampHeightPoints[7], -t_size.y))
+	## X-
+	vertices.push_back(Vector3(-t_size.x, 0, -t_size.y))
+	vertices.push_back(Vector3(0, 0, -t_size.y))
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[8], -t_size.y))
+	
+	######### BACK
+	#### X-
+	## X-
+	vertices.push_back(Vector3(0, rampHeightPoints[1], t_size.y))
+	vertices.push_back(Vector3(-t_size.x, 0, t_size.y))
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[2], t_size.y))
+	## X+
+	vertices.push_back(Vector3(0, 0, t_size.y))
+	vertices.push_back(Vector3(-t_size.x, 0, t_size.y))
+	vertices.push_back(Vector3(0, rampHeightPoints[1], t_size.y))
+	#### X+
+	## X-
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[0], t_size.y))
+	vertices.push_back(Vector3(0, 0, t_size.y))
+	vertices.push_back(Vector3(0, rampHeightPoints[1], t_size.y))
+	## X+
+	vertices.push_back(Vector3(t_size.x, 0, t_size.y))
+	vertices.push_back(Vector3(0, 0, t_size.y))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[0], t_size.y))
+	
+	######### LEFT
+	#### Z-
+	## Z-
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[5], 0))
+	vertices.push_back(Vector3(-t_size.x, 0, -t_size.y))
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[8], -t_size.y))
+	## Z+
+	vertices.push_back(Vector3(-t_size.x, 0, -t_size.y))
+	vertices.push_back(Vector3(-t_size.x, 0, 0))
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[5], -t_size.y))
+	#### Z+
+	## Z-
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[2], t_size.y))
+	vertices.push_back(Vector3(-t_size.x, 0, 0))
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[5], 0))
+	## Z+
+	vertices.push_back(Vector3(-t_size.x, 0, t_size.y))
+	vertices.push_back(Vector3(-t_size.x, 0, 0))
+	vertices.push_back(Vector3(-t_size.x, rampHeightPoints[2], t_size.y))
+	
+	######### RIGHT
+	#### Z+
+	## Z+
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[3], 0))
+	vertices.push_back(Vector3(t_size.x, 0, t_size.y))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[0], t_size.y))
+	## Z-
+	vertices.push_back(Vector3(t_size.x, 0, 0))
+	vertices.push_back(Vector3(t_size.x, 0, t_size.y))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[3], 0))
+	#### Z-
+	## Z+
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[6], -t_size.y))
+	vertices.push_back(Vector3(t_size.x, 0, 0))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[3], 0))
+	## Z-
+	vertices.push_back(Vector3(t_size.x, 0, -t_size.y))
+	vertices.push_back(Vector3(t_size.x, 0, 0))
+	vertices.push_back(Vector3(t_size.x, rampHeightPoints[6], -t_size.y))
+	
 	
 	shape.set_faces(vertices)
 	collisionShape.shape = shape
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
